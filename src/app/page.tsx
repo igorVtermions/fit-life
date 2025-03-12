2636f1e0eae92965aca8af490e8c03b8fd145ac6
anthonyhfranca <aharisson@uolinc.com>
2025-03-12 20:17:20 -0300

    Criando infoblocks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,17 @@ import Banner from "@/components/Banner";
 import Header from "@/components/Header";
 import InfoBlock from "@/components/InfoBlock";
 
+
+import WomanWorkingOut from '../components/InfoBlock/assets/womanWorkingOut.png';
+import womanMeditating from '../components/InfoBlock/assets/womanMeditating.png';
+
 export default function Home() {
   return (
     <section className="max-w-[1280px] mx-auto">
       <Header />
       <Banner />
-      <InfoBlock />
+      <InfoBlock reverse={false} buttonText={'Entre pro time!'} image={WomanWorkingOut} title={'Get more with low-cost training programs and advanced features.'} subtitle={'We believe fitness should be accessible to everyone, everywhere, regardless of income or access to a gym. With hundreds of professional workouts, healthy recipes and informative articles, as well as one of the most positive communities on the web, you’ll have everything you need to reach your personal fitness goals – for free!'}/>
+      <InfoBlock reverse={true} buttonText={'Entre pro time!'} image={womanMeditating} title={'Get more with low-cost training programs and advanced features.'} subtitle={'We believe fitness should be accessible to everyone, everywhere, regardless of income or access to a gym. With hundreds of professional workouts, healthy recipes and informative articles, as well as one of the most positive communities on the web, you’ll have everything you need to reach your personal fitness goals – for free!'}/>
     </section>
   );
 }
