12c4896cb8d36204adfd6ef2bf296c80f77b7f43
anthonyhfranca <aharisson@uolinc.com>
2025-03-13 22:14:43 -0300

    fazendo bloco de planos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import InfoBlock from "@/components/InfoBlock";
 
 import WomanWorkingOut from '../components/InfoBlock/assets/womanWorkingOut.png';
 import womanMeditating from '../components/InfoBlock/assets/womanMeditating.png';
+import Membership from "@/components/Membership";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <Banner />
       <InfoBlock reverse={false} buttonText={'Entre pro time!'} image={WomanWorkingOut} title={'Get more with low-cost training programs and advanced features.'} subtitle={'We believe fitness should be accessible to everyone, everywhere, regardless of income or access to a gym. With hundreds of professional workouts, healthy recipes and informative articles, as well as one of the most positive communities on the web, you’ll have everything you need to reach your personal fitness goals – for free!'}/>
       <InfoBlock reverse={true} buttonText={'Entre pro time!'} image={womanMeditating} title={'Get more with low-cost training programs and advanced features.'} subtitle={'We believe fitness should be accessible to everyone, everywhere, regardless of income or access to a gym. With hundreds of professional workouts, healthy recipes and informative articles, as well as one of the most positive communities on the web, you’ll have everything you need to reach your personal fitness goals – for free!'}/>
+      <Membership />
     </section>
   );
 }
