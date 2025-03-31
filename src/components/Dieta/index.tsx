def6d616d8dc52417bc5b65ef3df07585b9bc606
anthonyhfranca <aharisson@uolinc.com>
2025-03-31 17:27:13 -0300

    Criando botões pra adicionar Rotina

--- src/components/Dieta/index.tsx
+++ src/components/Dieta/index.tsx
@@ -7,13 +7,16 @@ export default function Dieta() {
 const dietaCompleta = dieta.dieta
 
   return (
-    <section className='w-full flex max-w-[1280px] overflow-auto gap-4'>
+    <section>
+      <button className='bg-orange-700 w-[170px] h-[50px] rounded-md hover:bg-orange-600 cursor-pointer my-8'>Adicionar refeição</button>
+    <div className='w-full flex max-w-[1280px] overflow-auto gap-4'>
         {dietaCompleta.days.map((dieta) => (<div key={dieta.day} className='bg-zinc-900 min-w-[340px] p-2.5 rounded-sm'>
             <h2 className='text-xl font-bold uppercase'>{dieta.day}</h2>
             <ul className='flex flex-col gap-4'>
                 {dieta.meals.map((meals) => (<li key={meals.meal} className='hover:bg-zinc-800 pl-1 py-1 rounded-sm'><span>{meals.time} - </span>{meals.meal}</li>))}
             </ul>
         </div>))}
+    </div>
     </section>
   )
 }
--- src/components/Treino/index.tsx
+++ src/components/Treino/index.tsx
@@ -7,7 +7,9 @@ export default function Treino() {
   const treinoCompleto = treino.treino
 
   return (
-    <section className='w-full flex max-w-[1280px] overflow-auto gap-4'>
+    <section>
+    <button className='bg-orange-700 w-[170px] h-[50px] rounded-md hover:bg-orange-600 cursor-pointer my-8'>Adicionar exercício</button>
+    <div className='w-full flex max-w-[1280px] overflow-auto gap-4'>
         {treinoCompleto.days.map((treino) => (<div key={treino.day} className='bg-zinc-900 min-w-[340px] p-2.5 rounded-sm'>
             <h2 className='text-xl font-bold uppercase'>{treino.day}</h2>
             <p className='mb-5 text-sm text-zinc-400'>{treino.exercises[0].exercicio}</p>
@@ -15,6 +17,7 @@ export default function Treino() {
                 {treino.exercises[0].exercicios.map((exercise) => (<li key={exercise} className='hover:bg-zinc-800 pl-1 py-1 rounded-sm'>{exercise}</li>))}
             </ul>
         </div>))}
+    </div>
     </section>
   )
 }
