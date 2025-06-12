e74045551d2f9190e90d686ab01a319ca14c30b2
anthonyh30 <anthonyharisson30@gmail.com>
2025-06-11 21:18:35 -0300

    chore: add prettier and eslint setup

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,21 +1,24 @@
 import Dieta from "@/components/Dieta";
 import Header from "@/components/Header";
 import Treino from "@/components/Treino";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <section className="w-full flex flex-col items-center mt-20 px-5">
-        <Tabs defaultValue="account" className="w-full">
+      <section className="mt-20 flex w-full flex-col items-center px-5">
+        <Tabs defaultValue="dieta" className="w-full">
           <TabsList className="w-full">
             <TabsTrigger value="dieta">Minha Dieta</TabsTrigger>
             <TabsTrigger value="treino">Meu Treino</TabsTrigger>
           </TabsList>
-          <TabsContent value="dieta" className="w-full"><Dieta /></TabsContent>
-          <TabsContent value="treino"><Treino /></TabsContent>
+          <TabsContent value="dieta" className="w-full">
+            <Dieta />
+          </TabsContent>
+          <TabsContent value="treino">
+            <Treino />
+          </TabsContent>
         </Tabs>
       </section>
     </>
