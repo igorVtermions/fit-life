2839cc7fa19e3f8329177421c45637691b46e1ed
anthonyh30 <anthonyharisson30@gmail.com>
2025-06-23 02:05:47 -0300

    refactor: removing dayjs dependency - no needed anymore

--- src/actions/upsert-meal/index.ts
+++ src/actions/upsert-meal/index.ts
@@ -6,22 +6,10 @@ import { mealsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { actionClient } from "@/lib/next-safe-action";
-import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
-
-dayjs.extend(utc);
 
 export const upsertMeal = actionClient
   .schema(upsertMealSchema)
   .action(async ({ parsedInput }) => {
-    const timeToMeal = parsedInput.timeToMeal;
-
-    const timeToMealUTC = dayjs()
-      .set("hour", parseInt(timeToMeal.split(":")[0]))
-      .set("minute", parseInt(timeToMeal.split(":")[1]))
-      .set("second", parseInt(timeToMeal.split(":")[2]))
-      .utc();
-
     const session = await auth.api.getSession({
       headers: await headers(),
     });
@@ -37,13 +25,11 @@ export const upsertMeal = actionClient
         ...parsedInput,
         id: parsedInput.id,
         goalId: session?.user.goal?.id,
-        timeToMeal: timeToMealUTC.format("HH:mm:ss"),
       })
       .onConflictDoUpdate({
         target: [mealsTable.id],
         set: {
           ...parsedInput,
-          timeToMeal: timeToMealUTC.format("HH:mm:ss"),
         },
       });
   });
--- src/components/Dieta/index.tsx
+++ src/components/Dieta/index.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 
 import AddMealButton from "@/app/home/_components/add-meal-button";
+import { headers } from "next/headers";
+import { eq } from "drizzle-orm";
+import { mealsTable } from "@/db/schema";
+import { db } from "@/db";
+import { auth } from "@/lib/auth";
 
-export default function Dieta() {
+export default async function Dieta() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  const meals = await db.query.mealsTable.findMany({
+    where: eq(mealsTable.goalId, session?.user.goal?.id),
+  });
   return (
     <section>
       <AddMealButton />
-      <div className="flex w-full max-w-[1280px] gap-4 overflow-auto">{}</div>
+      <div className="flex w-full max-w-[1280px] gap-4 overflow-auto">
+        {meals.map((meal) => (
+          <li>
+            {meal.timeToMeal.slice(0, 5)} - {meal.mealName}
+          </li>
+        ))}
+      </div>
     </section>
   );
 }
