9ccad1c8fe7e722c1617b50f78b07f87c7ebd4f4
anthonyh30 <anthonyharisson30@gmail.com>
2025-06-23 02:26:01 -0300

    feat: adding edit and delete btn to meals

--- src/app/home/_components/card/index.tsx
+++ src/app/home/_components/card/index.tsx
@@ -1,10 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { SquarePen, Trash2 } from "lucide-react";
 import React from "react";
 
-export default function Card() {
+export default function Card({ day, title, meals }: any) {
   return (
     <div className="min-w-[340px] rounded-sm bg-zinc-900 p-2.5">
-      <h2 className="text-xl font-bold uppercase">{}</h2>
-      <ul className="flex flex-col gap-4">{}</ul>
+      <h2 className="text-xl font-bold uppercase">{title}</h2>
+      <ul className="flex flex-col gap-4">
+        {meals
+          .filter((meal: any) => meal.day == day.toLowerCase())
+          .map((diet: any) => (
+            <li
+              key={diet.timeToMeal}
+              className="flex items-center justify-between rounded-sm border border-orange-600/20 py-1 pl-1 hover:bg-zinc-800"
+            >
+              <span>
+                {diet.timeToMeal.slice(0, 5)} - {diet.mealName}
+              </span>
+              <div className="flex gap-2">
+                <Button className="cursor-pointer bg-orange-600 hover:bg-orange-700">
+                  <SquarePen />
+                </Button>
+                <Button className="cursor-pointer bg-orange-600 hover:bg-orange-700">
+                  <Trash2 />
+                </Button>
+              </div>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
--- src/components/Dieta/index.tsx
+++ src/components/Dieta/index.tsx
@@ -6,23 +6,37 @@ import { eq } from "drizzle-orm";
 import { mealsTable } from "@/db/schema";
 import { db } from "@/db";
 import { auth } from "@/lib/auth";
+import Card from "@/app/home/_components/card";
 
 export default async function Dieta() {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
 
+  if (!session?.user?.goal) {
+    throw new Error("Goal not found");
+  }
+
   const meals = await db.query.mealsTable.findMany({
     where: eq(mealsTable.goalId, session?.user.goal?.id),
   });
+
+  const weekDays = [
+    "Domingo",
+    "Segunda",
+    "Terca",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sabado",
+  ];
+
   return (
     <section>
       <AddMealButton />
       <div className="flex w-full max-w-[1280px] gap-4 overflow-auto">
-        {meals.map((meal) => (
-          <li>
-            {meal.timeToMeal.slice(0, 5)} - {meal.mealName}
-          </li>
+        {weekDays.map((day) => (
+          <Card key={day} title={day} day={day} meals={meals} />
         ))}
       </div>
     </section>
